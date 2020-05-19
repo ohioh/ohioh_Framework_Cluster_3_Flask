1d63ae8b460c669753c033512f446dea4c74d42a
Tjark Ziehm <63347689+ohioh@users.noreply.github.com>
2020-05-19 16:41:01 +0200

    go wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,11 @@
+import os
+from main import create_app
+from config import Config
+from dotenv import load_dotenv
 
+load_dotenv()
+
+app = create_app(Config)
+
+if __name__ == '__main__':
+    app.run()
