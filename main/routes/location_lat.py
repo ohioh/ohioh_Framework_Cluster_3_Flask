b0eb78b2a69bc542a92df2bd08dfac11a527ac0d
Codefred <alfredemmanuelinyang@gmail.com>
2020-06-05 03:55:00 +0100

    fixed schema

--- main/routes/location_lat.py
+++ main/routes/location_lat.py
@@ -4,7 +4,7 @@ from main import mongo
 from flask_restful import Resource
 from ..schemas.location_lat import LocationLatSchema
 from ..services.db import DbOperations
-
+from bson.objectid import ObjectId
 
 location_lat = mongo.ohioh.location_lat
 db = DbOperations(collections=location_lat, schema=LocationLatSchema)
@@ -22,17 +22,17 @@ class LocationLatList(Resource):
 class LocationLat(Resource):
     def get(self, location_id):
         return db.find_one(
-            criteria={'location_id': location_id}
+            criteria={'_id': ObjectId(location_id)}
         )
 
     def put(self, location_id):
         payload = request.get_json()
         return db.update(
-            criteria={'location_id': location_id},
+            criteria={'_id': ObjectId(location_id)},
             updated_value=payload
         )
 
     def delete(self, location_id):
         return db.delete(
-            criteria={'location_id': location_id}
+            criteria={'_id': ObjectId(location_id)}
         )
