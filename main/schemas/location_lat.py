6676116dc6d959462957962577623e4dd47fb7e7
Codefred <alfredemmanuelinyang@gmail.com>
2020-06-11 14:46:02 +0100

    New update

--- main/schemas/location_lat.py
+++ main/schemas/location_lat.py
@@ -7,7 +7,10 @@ import marshmallow.fields as ms_fields
 class LocationLatSchema(Schema):
     latitude = ms_fields.Float()
     departure = ms_fields.Bool()
+
+    
     class Meta:
         unknown = EXCLUDE
+
 
 
